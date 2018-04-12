ed959e33ed295f76b247fe1c7c7a75e532906476
asherlie <lieber.asher@gmail.com>
2018-04-12 00:27:29 -0400

    added gaps and columns from dwm.suckless.org/patches, added iter function to search and grab clients matching an argument

--- config.h
+++ config.h
@@ -1,25 +1,29 @@
 /* See LICENSE file for copyright and license details. */
-
 /* appearance */
+//#include <X11/XF86keysym.h>
+//#define ROMANNUM
+static const char *fonts[] = {
+	"monospace:size=11"
+};
+static const unsigned int gappx = 8;
+static const char dmenufont[]       = "monospace:size=10";
+static const char normbordercolor[] = "#444444";
+static const char normbgcolor[]     = "#222222";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char selbordercolor[]  = "#005577";
+static const char selbgcolor[]      = "#005577";
+static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+#ifdef ROMANNUM
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+#else
+static const char *tags[] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+#endif
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -41,6 +45,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+      { "|||",      col  },
 };
 
 /* key definitions */
@@ -55,14 +60,43 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+static const char* mutecmd[] = { "/home/asher/bin/alsa_t/alsa_t", NULL };
+static const char* vol_up_cmd[]  = { "amixer", "-q", "sset", "Master", "10%+", NULL };
+static const char* vol_dwn_cmd[] = { "amixer", "-q", "sset", "Master", "10%-", NULL };
+static const char* rs_toggle_cmd[] = { "/home/asher/bin/rs_t/rs_t", "1500", "6500", "t", NULL }; // the lower the first arg, the redder
+static const char* rs_inc_cmd[] = {"/home/asher/bin/rs_t/rs_t", "1500", "6500", "i", NULL };
+static const char* rs_dec_cmd[] = {"/home/asher/bin/rs_t/rs_t", "1500", "6500", "d", NULL };
+static const char* mpc_toggle_cmd[] = { "mpc", "toggle", NULL };
+static const char* mpc_prev_cmd[] = { "mpc", "prev", NULL };
+static const char* mpc_next_cmd[] = { "mpc", "next", NULL };
+//static const char* reboot_cmd[] = { "reboot", "null" };
+static const char* browser_cmd[] = { "chromium", NULL };
+static const char* incog_browser_cmd[] = { "chromium", "--incognito", NULL };
+//static const char* rshift_mr[] = {"/home/asher/bin/"};
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]  = { "xfce4-terminal", NULL };
+static const char *find_str[]  = { "Chromium", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+      { 0,                            0x1008ff12,spawn,          {.v = mutecmd } },
+      { 0,                            0x1008ff11,spawn,          {.v = vol_dwn_cmd } },
+      { 0,                            0x1008ff13,spawn,          {.v = vol_up_cmd } },
+      { 0,                            0xffbe,    spawn,          {.v = rs_toggle_cmd } },
+      { 0,                            XK_F11,    spawn,          {.v = rs_inc_cmd } },
+      { 0,                            XK_F9,     spawn,          {.v = rs_dec_cmd } },
+      { 0,                            XK_F10,    spawn,          {.v = rs_toggle_cmd } },
+      //{ 0,                            0x1008ff2d,spawn,          {.v = reboot_cmd } },
+      { 0,                            0x1008ff41,spawn,          {.v = mpc_toggle_cmd } },
+      { 0,                            0x1008ff16,spawn,          {.v = mpc_prev_cmd } },
+      { 0,                            0x1008ff17,spawn,          {.v = mpc_next_cmd } },
+      { 0,                            0x1008ff14,spawn,          {.v = mpc_toggle_cmd } },
+      //{ 0,                            0x1008ffb2,quit,           {0} },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             0x5c,      spawn,          {.v = browser_cmd } },
+	{ MODKEY|ShiftMask,             0x5d,      spawn,          {.v = incog_browser_cmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -76,6 +110,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_s,      iter,           {.v = find_str} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
